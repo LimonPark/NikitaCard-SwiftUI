4430ccfa2741a4b764a702e4de9f91318d0bfb2c
nikita <kafes-nik@mail.ru>
2023-09-12 19:29:55 +0300

    Initial Commit

---
+++ NikitaCard/NikitaCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  NikitaCardApp.swift
+//  NikitaCard
+//
+//  Created by Nikita Hasan Kafes on 12.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct NikitaCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
